67ab4cf07d223a93c618fa9ed0e6b9b3175ce9aa
penaf05-sys <penaf05@gmail.com>
2026-05-18 15:28:53 -0400

    Fix server.py clean rewrite

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-cat > ~/Documents/cigar-project/server.py << 'EOF'
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import anthropic
@@ -61,4 +60,3 @@ def get_story():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
-EOF
